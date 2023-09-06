ed8ca465323b901a03fb6c3832eb91f50e952298
RyotaOku <ryota1991o@gmail.com>
2023-09-06 12:25:20 +0900

    スタートページを追加し編集

---
+++ src/components/smartphoneApp/startPage/Footer.tsx
@@ -0,0 +1,19 @@
+import { log } from 'console'
+import { useState } from 'react'
+import style from '@/styles/components/smartphoneApp/startPage/footer.module.css'
+import styles from '@/styles/components/smartphoneApp/startPage/header.module.css'
+
+export type props = {
+    text: string
+}
+
+export default function Footer() {
+    return (
+        <>
+            <div className={style.wrap}>
+
+
+            </div>
+        </>
+    )
+}
--- src/pages/smartphoneApp/startPage/index.tsx
+++ src/pages/smartphoneApp/startPage/index.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
-
+import Footer from '@/components/smartphoneApp/startPage/Footer'
+import style from '@/styles/components/smartphoneApp/startPage/main.module.css'
 
 export default function Page() {
     return (
-        <>
-            <main>スマホのページ</main>
-            <footer></footer>
-        </>
+        <div className={style.wrap}>
+            <main className={style.mainWrap}>
+                <h1>終電なくてももう安心。</h1>
+                <p>UniRideで割り勘しちゃおう。</p>
+                <p>好きなスポットを選んでマッチングしよう。</p>
+
+            </main>
+            <Footer />
+        </div>
     )
 }
