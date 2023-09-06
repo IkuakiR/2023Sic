8f7017767d541574cd53b284b234fca8b52c72d9
RyotaOku <ryota1991o@gmail.com>
2023-09-06 11:57:29 +0900

    フォルダ追加

---
+++ src/pages/smartphoneApp/startPage/index.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+
+export default function Page() {
+    return (
+        <>
+            <main>スマホのページ</main>
+            <footer></footer>
+        </>
+    )
+}
