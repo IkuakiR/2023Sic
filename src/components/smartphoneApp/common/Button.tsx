d2b0743d104642670acf0c32303f71313c3217c3
RyotaOku <ryota1991o@gmail.com>
2023-09-07 09:54:04 +0900

    buttonコンポーネント変更

--- src/components/smartphoneApp/common/Button.tsx
+++ src/components/smartphoneApp/common/Button.tsx
@@ -17,8 +17,21 @@ export default function Button(props: ButtonProps) {
     let className = `${baseClassName} ${additionalClassName}`;
 
     const handleClick = () => {
-        if (!props.disabled) {
-            window.location.href = props.link
+        // ボタンが無効な場合、何もしない
+        if (props.disabled) {
+            return;
+        }
+
+        // linkプロパティが指定されている場合、そのURLに遷移する
+        if (props.link) {
+            window.location.href = props.link;
+            return;
+        }
+
+        // onClickプロパティが指定されている場合、その関数を呼び出す
+        if (props.onClick) {
+            props.onClick();
+            return;
         }
     }
 
