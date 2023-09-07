cb075f69a17b085ea766427226ff13d508fe28cb
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-09-07 10:07:16 +0900

    Phone

---
+++ src/components/smartphoneApp/startPage/Phone.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import style from "@/styles/components/smartphoneApp/startPage/accountRegistration/phone.module.css";
+
+export function Phone() {
+  return (
+    <>
+      <div className={style.phone}></div>
+    </>
+  );
+}
--- src/pages/smartphoneApp/startPage/index.tsx
+++ src/pages/smartphoneApp/startPage/index.tsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
-import Footer from '@/components/smartphoneApp/startPage/Footer'
-import style from '@/styles/components/smartphoneApp/startPage/main.module.css'
-import { Frame } from '@/components/smartphoneApp/common/Frame'
+import { useState } from "react";
+import Footer from "@/components/smartphoneApp/startPage/Footer";
+import style from "@/styles/components/smartphoneApp/startPage/main.module.css";
+import { Phone } from "@/components/smartphoneApp/startPage/Phone";
+import { Frame } from "@/components/smartphoneApp/common/Frame";
 
 export default function Page() {
-    return (
-        <Frame>
-            <main className={style.mainWrap}>
-                <div className={style.titleWrap}>
-                    <h1>終電なくてももう安心。</h1>
-                    <p>UniRideで割り勘しちゃおう。</p>
-                    <p>好きなスポットを選んでマッチングしよう。</p>
+  return (
+    <Frame>
+      <main className={style.mainWrap}>
+        <div className={style.titleWrap}>
+          <h1>終電なくてももう安心。</h1>
+          <p>UniRideで割り勘しちゃおう。</p>
+          <p>好きなスポットを選んでマッチングしよう。</p>
+        </div>
 
-                </div>
-
-            </main>
-            <Footer />
-        </Frame>
-    )
+        <Phone />
+      </main>
+      <Footer />
+    </Frame>
+  );
 }
