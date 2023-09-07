c628bdc6877e916873ce0c0b12254eabece8b82a
RyotaOku <ryota1991o@gmail.com>
2023-09-07 09:39:02 +0900

    インデント調整

--- src/components/smartphoneApp/startPage/Footer.tsx
+++ src/components/smartphoneApp/startPage/Footer.tsx
@@ -3,23 +3,23 @@ import style from "@/styles/components/smartphoneApp/startPage/footer.module.css
 import Button from "@/components/smartphoneApp/common/Button";
 
 export type props = {
-  text: string;
+    text: string;
 };
 
 export default function Footer() {
-  return (
-    <>
-      <div className={style.wrap}>
-        <Button
-          disabled={false}
-          text={"UniRideを利用する"}
-          mainColor={true}
-          link={"/smartphoneApp/startPage/accountRegistration"}
-        />
-        <a href="#" className={style.already}>
-          既にアカウントをお持ちの方
-        </a>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={style.wrap}>
+                <Button
+                    disabled={false}
+                    text={"UniRideを利用する"}
+                    mainColor={true}
+                    link={"/smartphoneApp/startPage/accountRegistration"}
+                />
+                <a href="#" className={style.already}>
+                    既にアカウントをお持ちの方
+                </a>
+            </div>
+        </>
+    );
 }
