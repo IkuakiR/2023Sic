26e68d1bfcf6352deb7c3bbc95ba34a604518f21
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-09-07 09:12:29 +0900

    aaaa

--- src/components/smartphoneApp/startPage/Footer.tsx
+++ src/components/smartphoneApp/startPage/Footer.tsx
@@ -1,27 +1,25 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import style from '@/styles/components/smartphoneApp/startPage/footer.module.css'
-import Button from '@/components/smartphoneApp/common/Button'
-=======
-import { log } from "console";
 import { useState } from "react";
 import style from "@/styles/components/smartphoneApp/startPage/footer.module.css";
-import styles from "@/styles/components/smartphoneApp/startPage/header.module.css";
-import Button from "@/components/smartphoneApp/startPage/Button";
->>>>>>> e842aa5 (create button)
+import Button from "@/components/smartphoneApp/common/Button";
 
 export type props = {
   text: string;
 };
 
 export default function Footer() {
-    return (
-        <>
-            <div className={style.wrap}>
-                <Button disabled={false} text={'UniRideを利用する'} mainColor={true} link={'/smartphoneApp/startPage/accountRegistration'} />
-                <a href='#' className={style.already}>既にアカウントをお持ちの方</a>
-
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className={style.wrap}>
+        <Button
+          disabled={false}
+          text={"UniRideを利用する"}
+          mainColor={true}
+          link={"/smartphoneApp/startPage/accountRegistration"}
+        />
+        <a href="#" className={style.already}>
+          既にアカウントをお持ちの方
+        </a>
+      </div>
+    </>
+  );
 }
